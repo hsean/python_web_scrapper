917c0c7072bb58b12a155616ae33ff2dfb61f968
Sean Hendrickson <henamber3@aol.com>
2021-09-25 02:08:38 -0700

    pretty display script
    creating script to improve readability of web scrapper output

--- pretty_display.py
+++ pretty_display.py
@@ -9,12 +9,34 @@
 
 # store text file
 lines = []
+titles = []
+prices = []
 
+
+# TODO: find longest title in list
+# TODO: sum all prices
 # read data from a text file
 with open('output.txt', 'r') as f:
     lines = f.readlines()
 
     count = 0
     for line in lines:
-        count += 1
-        print(f'line {count}: {line}')
+        if line.startswith('$'):
+            prices.append(line)
+        elif line == "\n":
+            pass
+        else:
+            titles.append(line)
+            count = count + 1
+
+    # display data
+    print("lines array")
+    print(lines)
+    print("The list of titles is:")
+    print(titles)
+    print("The list of prices is:")
+    print(prices)
+
+    # display each item
+    for x in count:
+        print(x)
